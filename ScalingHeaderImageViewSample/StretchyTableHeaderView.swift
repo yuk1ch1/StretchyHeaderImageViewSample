a1d8cf2d7ec95538ab5af48c121cfa04dcd5ec37
山田 諭 <sma2ww@gmail.com>
2021-12-15 22:36:35 +0900

    impl: TableHeaderView

---
+++ ScalingHeaderImageViewSample/StretchyTableHeaderView.swift
@@ -0,0 +1,44 @@
+import UIKit
+
+class StretchyTableHeaderView: UIView {
+    static let imageHeihgt = 328.3333333333333
+    private var imageViewHeight = NSLayoutConstraint()
+    private var imageViewBottom = NSLayoutConstraint()
+
+    private lazy var imageView: UIImageView = {
+        let view = UIImageView(image: UIImage(named: "headerImage"))
+        view.backgroundColor = .yellow
+        view.contentMode = .scaleAspectFill
+        return view
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        setConstraints()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+
+    func setConstraints() {
+        self.addAutoLayoutedSubview(imageView)
+        imageViewBottom = imageView.bottomAnchor.constraint(equalTo: self.bottomAnchor)
+        imageViewHeight = imageView.heightAnchor.constraint(equalTo: self.heightAnchor)
+
+        NSLayoutConstraint.activate([
+            imageView.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            imageViewBottom,
+            imageViewHeight,
+        ])
+    }
+
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+        imageViewHeight.constant = scrollView.contentInset.top
+        let offsetY = -(scrollView.contentOffset.y + scrollView.contentInset.top)
+        imageView.clipsToBounds = offsetY <= 0
+        imageViewBottom.constant = offsetY >= 0 ? 0 : -offsetY/2
+        imageViewHeight.constant = max(offsetY + scrollView.contentInset.top, scrollView.contentInset.top)
+    }
+}
